4a257d16991e2672c987a4423012a3adca0277c0
Cliff Xuan <cliff.xuan@gmail.com>
2019-01-19 22:11:44 +0000

    test case conversion

--- mew/convert_case.py
+++ mew/convert_case.py
@@ -1,17 +1,33 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import re
+
+CAP_RE = re.compile('(.)([A-Z])')
 
 
 def to_pascal_case(name):
     """
-    Turn a Pythonic attribute name into PascalCase.
+    Turn a snake_case name into PascalCase.
     """
     return name.title().replace("_", "")
 
 
 def from_pascal_case(name):
     """
-    Turn a PascalCase attribute name into Pythonic name.
+    Turn a PascalCase name into snake_case.
     """
-    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
-    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
+    return CAP_RE.sub(r'\1_\2', name).lower()
+
+
+def to_camel_case(name):
+    """
+    Turn a snake_case name into camelCase.
+    """
+    output = name.title().replace("_", "")
+    return output[0].lower() + output[1:]
+
+
+def from_camel_case(name):
+    """
+    Turn a camelCase name into snake_case.
+    """
+    return from_pascal_case(name)
